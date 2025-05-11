f7a504b18fc239bbdd282141c0e2a45074d622e7
Jesus G <113757515+py3user22@users.noreply.github.com>
2025-05-11 02:02:35 -0500

    511 save21

--- my_data_sciences/notes1/MS_notes_py3_funds_510.py
+++ my_data_sciences/notes1/MS_notes_py3_funds_510.py
@@ -1,3 +1,27 @@
-"""051025 notes for data sciences w/ python """
+"""051125 notes for data sciences w/ python """
 
 
+Microsoft_python_dev = {
+    0: 'https://www.coursera.org/programs/m-uq19a/professional-certificates/microsoft-python-developer?',
+    0.1: '------------------  in progress ',
+    1: '24hr -- Python programming fundamentals',
+
+        # todo
+    2: '20hr -- Data analysis & visualization w/ python',
+    3: '26hr -- Automation & Scripting w/ python',
+    4: '22hr -- Web development w/ python',
+    5: '21hr -- Advanced python development techniques',
+    6: '19hr -- Project development w/ python',
+}
+
+#//////////////////////////////////// notes
+
+"""
+# install jupyter notebook using pip
+>> pip install notebook
+
+# to open jupyter notebook on cmd line
+>> jupyter notebook 
+jupyter notebook
+
+"""
