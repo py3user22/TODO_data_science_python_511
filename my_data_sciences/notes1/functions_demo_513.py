e9669b398a1d486a0fdcd073303eb618746a90ea
Jesus G <113757515+py3user22@users.noreply.github.com>
2025-05-13 01:30:48 -0500

    513 save 2

---
+++ my_data_sciences/notes1/functions_demo_513.py
@@ -0,0 +1,39 @@
+"""051325 functions demo"""
+
+
+#/////////////////////////////
+"""
+# syntax of functions
+---------------------
+
+def function_name(parameters):
+    \"\"\"docstring explaining what function does\"\"\"
+    # code to perform the task
+    return result   # return statement to close
+"""
+
+#ex1 rectangle area
+def calculate_rectangle_area(length, width):
+  """Calculates the area of a rectangle.
+
+  Args:
+    length: The length of the rectangle.
+    width: The width of the rectangle.
+
+  Returns:
+    The area of the rectangle.
+  """
+  # Your code here
+  area = length * width
+  return area
+
+
+#/////////////////////////////
+
+
+
+
+
+#/////////////////////////////
+#/////////////////////////////
+#/////////////////////////////
